9aee2976bcec8e73cd7154f6d050d530a399532f
fevzi-coder <fevzi.pobeditel@gmail.com>
2021-04-14 20:38:43 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,11 @@ $(function () {
     autoplaySpeed: 2000
   }) 
 
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#CCCCCE",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
 });
